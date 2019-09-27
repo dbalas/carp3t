b557637b03d158f0fdbbf42d50ef7c381f475a23
Daniel Balastegui <daniel.balastegui@forcemanager.net>
2019-09-27 12:41:41 +0200

    finishing form and favicons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="carp3t-app">
         <carp3t-form></carp3t-form>
     </div>
 </template>
@@ -18,11 +18,37 @@ export default {
 @import '@/assets/variables';
 @import 'node_modules/bootstrap/scss/bootstrap.scss';
 
-#app {
+#carp3t-app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 60px;
+
+    textarea:focus,
+    button:focus,
+    input[type='text']:focus,
+    input[type='password']:focus,
+    input[type='datetime']:focus,
+    input[type='datetime-local']:focus,
+    input[type='date']:focus,
+    input[type='month']:focus,
+    input[type='time']:focus,
+    input[type='week']:focus,
+    input[type='number']:focus,
+    input[type='email']:focus,
+    input[type='url']:focus,
+    input[type='search']:focus,
+    input[type='tel']:focus,
+    input[type='color']:focus,
+    .uneditable-input:focus {
+        outline: 0;
+        outline: thin dotted \9;
+        box-shadow: none !important;
+    }
+
+    input::placeholder {
+        color: #e0e0e0 !important;
+    }
 }
 </style>
--- src/components/Carp3tForm.vue
+++ src/components/Carp3tForm.vue
@@ -1,85 +1,254 @@
 <template>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group :label="$t('material')" class="mb-0">
+        <b-form-group :label="$t('Material')" class="mb-0">
+            <span class="price-label">{{ totals.material.toFixed(2) }}€</span>
+            <b-form-group
+                id="material-quantity-group"
+                :label="$t('Quantity')"
+                label-for="materialQuantity"
+                label-cols-sm="4"
+            >
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>g</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="materialQuantity"
+                        v-model="form.materialQuantity"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
             <b-form-group
                 id="material-group"
-                :label="$t('cost')"
+                :label="$t('Cost')"
                 label-for="material"
                 label-cols-sm="4"
             >
-                <span class="price-label">{{ 500 }}€</span>
-                <b-form-input
-                    id="material"
-                    v-model="form.material"
-                    type="text"
-                    required
-                    placeholder="€/kg"
-                ></b-form-input>
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>€/kg</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="materialCost"
+                        v-model="form.materialCost"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
             </b-form-group>
         </b-form-group>
-        <b-form-group :label="$t('electricity')" class="mb-0">
-            <b-form-group
-                label-cols-sm="4"
-                :label="$t('cost')"
-                label-for="electricityCost"
+        <b-form-group :label="$t('Electricity')" class="mb-0 mt-4">
+            <span class="price-label"
+                >{{ totals.electricity.toFixed(2) }}€</span
             >
-                <b-form-input
-                    id="electricityCost"
-                    v-model="form.electricityCost"
-                    type="text"
-                    required
-                    placeholder="€/kWh"
-                ></b-form-input>
-            </b-form-group>
-
             <b-form-group
                 label-cols-sm="4"
-                :label="$t('consumption')"
+                :label="$t('Consumption')"
                 label-for="consumption"
             >
-                <b-form-input
-                    id="consumption"
-                    v-model="form.consumption"
-                    type="text"
-                    required
-                    placeholder="W"
-                ></b-form-input> </b-form-group
-        ></b-form-group>
-
-        <div class="buttons">
-            <b-button class="submit-btn" type="submit" variant="primary"
-                >Calcular</b-button
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>W</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="consumption"
+                        v-model="form.consumption"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+            <b-form-group
+                label-cols-sm="4"
+                :label="$t('Cost')"
+                label-for="electricityCost"
             >
-            <b-button type="reset" variant="danger">Reset</b-button>
-        </div>
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>€/kWh</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="electricityCost"
+                        v-model="form.electricityCost"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+            <b-form-group
+                label-cols-sm="4"
+                :label="$t('Printing time')"
+                label-for="printingTime"
+            >
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>h</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="printingTime"
+                        v-model="form.printingTime"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+        </b-form-group>
+        <b-form-group :label="$t('Labor')" class="mb-0 mt-4">
+            <span class="price-label">{{ totals.labor.toFixed(2) }}€</span>
+            <b-form-group
+                label-cols-sm="4"
+                :label="$t('Processing time')"
+                label-for="processingHours"
+            >
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>h</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="processingHours"
+                        v-model="form.processingHours"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+            <b-form-group
+                label-cols-sm="4"
+                :label="$t('Price/hour')"
+                label-for="laborCost"
+            >
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>€/h</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="laborCost"
+                        v-model="form.laborCost"
+                        type="number"
+                        step="0.01"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+        </b-form-group>
+        <b-form-group :label="$t('Others')" class="mb-4 mt-4">
+            <span class="price-label">{{ totals.others.toFixed(2) }}€</span>
+            <b-form-group
+                label-cols-sm="4"
+                :label="$t('Failure rate')"
+                label-for="failureRate"
+            >
+                <b-input-group>
+                    <template v-slot:append>
+                        <b-input-group-text>%</b-input-group-text>
+                    </template>
+                    <b-form-input
+                        id="failureRate"
+                        v-model="form.failureRate"
+                        type="number"
+                    ></b-form-input>
+                </b-input-group>
+            </b-form-group>
+        </b-form-group>
+
+        <footer>
+            <b-button class="mr-3" type="submit" variant="outline-primary">{{
+                $t('Calculate')
+            }}</b-button>
+            <b-button type="reset" variant="outline-danger">{{
+                $t('Reset')
+            }}</b-button>
+            <span class="total-label"
+                >{{ $t('Total') }}: {{ totals.total.toFixed(2) }}€</span
+            >
+        </footer>
     </b-form>
 </template>
 
 <script>
+import Vue from 'vue';
 export default {
     name: 'Carp3tForm',
     data() {
         return {
             show: true,
+            totals: {
+                material: 0,
+                electricity: 0,
+                labor: 0,
+                others: 0,
+                total: 0,
+            },
             form: {
-                materialCost: '',
+                materialCost: null,
+                materialQuantity: null,
                 electricityCost: null,
                 consumption: null,
+                laborCost: null,
+                processingHours: null,
+                printingTime: null,
+                failureRate: null,
             },
         };
     },
     methods: {
         onSubmit(evt) {
+            this.totals = {
+                material:
+                    ((this.form.materialCost || 0) / 1000) *
+                    (this.form.materialQuantity || 0),
+                electricity:
+                    ((this.form.consumption || 0) / 1000) *
+                    (this.form.electricityCost || 0) *
+                    (this.form.printingTime || 0),
+                labor:
+                    (this.form.processingHours || 0) *
+                    (this.form.laborCost || 0),
+                others: 0,
+                total: 0,
+            };
+
+            Vue.set(
+                this.totals,
+                'others',
+                ((this.form.failureRate || 0) / 100) *
+                    (this.totals.material +
+                        this.totals.electricity +
+                        this.totals.labor)
+            );
+
+            Vue.set(
+                this.totals,
+                'total',
+                this.totals.material +
+                    this.totals.electricity +
+                    this.totals.labor +
+                    this.totals.others
+            );
+
             evt.preventDefault();
-            alert(JSON.stringify(this.form));
         },
         onReset(evt) {
             evt.preventDefault();
             // Reset our form values
-            this.form.email = '';
-            this.form.name = '';
-            this.form.food = null;
-            this.form.checked = [];
+            this.form = {
+                materialCost: null,
+                materialQuantity: null,
+                electricityCost: null,
+                consumption: null,
+                laborCost: null,
+                processingHours: null,
+                printingTime: null,
+                failureRate: null,
+            };
+            this.totals = {
+                material: 0,
+                electricity: 0,
+                labor: 0,
+                others: 0,
+                total: 0,
+            };
             // Trick to reset/clear native browser form validation state
             this.show = false;
             this.$nextTick(() => {
@@ -101,8 +270,8 @@ form {
     box-sizing: border-box;
     box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
 
-    .col-form-label {
-        text-transform: capitalize;
+    label.col-form-label {
+        font-size: 0.9rem;
     }
 
     .form-row > div {
@@ -118,9 +287,20 @@ form {
     }
 
     legend,
-    .price-label {
+    .price-label,
+    .total-label {
         font-variant: small-caps;
         color: theme-color('primary');
+    }
+
+    .input-group-text {
+        background-color: transparent;
+        color: #adadad;
+        border-left-color: transparent;
+    }
+
+    .form-control {
+        border-right-color: transparent;
     }
 
     .price-label {
@@ -129,14 +309,20 @@ form {
         right: 0;
         font-size: 0.95rem;
     }
-}
 
-.buttons {
-    margin-top: 1rem;
-    text-align: center;
-}
+    .total-label {
+        font-weight: bold;
+        font-variant: inherit;
+        font-size: 1rem;
+    }
 
-.submit-btn {
-    margin-right: 0.5rem;
+    footer {
+        display: flex;
+        align-items: center;
+        .total-label {
+            flex-grow: 1;
+            text-align: right;
+        }
+    }
 }
 </style>
